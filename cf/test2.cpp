0ca9b31ba6fdc348f5f4718c3d8cf1a6b28c37a1
mdmuneerhasan <md.muneerhasan@gmail.com>
2021-03-13 20:59:20 +0530

    20:59-13-03-2021

--- cf/test2.cpp
+++ cf/test2.cpp
@@ -1,67 +1,33 @@
-/* This code is submitted by Muneer Hasan
-* Computer-scince student ( @ Jamia Millia Islamia : New Delhi)
-* Email : md.muneerhasan@gmail.com
-* 20:01:41 Tuesday 09-March:2021*/
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-#define int             long long int
-#define SF(v)           sort(v.begin(),v.end())
-#define F               first
-#define S               second
-#define UP(x)           for(int i=0;i<x.size();i++){if(x[i]<='z' && x[i]>='a')x[i]=char(x[i]-'a'+'A');}
-#define LO(x)           for(int i=0;i<x.size();i++){if(x[i]<='Z' && x[i]>='A')x[i]=char(x[i]-'A'+'a');}
-#define MP              make_pair
-#define PII             pair<int, int>
-#define MAXHEAP         priority_queue<int>
-#define MINHEAP         priority_queue<int, vector<int>, greater<int> >
-#define SETBIT(x)       __builtin_popcountll(x)
-#define ZEROBIT(x)      __builtin_ctzll(x)
-#define INF             1e18
-#define PS(x,y)         fixed<<setprecision(y)<<x
-#define MOD             1000000007
-#define db(...)         __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename Arg1>void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }template <typename Arg1, typename... Args>void __f(const char* names, Arg1&& arg1, Args&&... args) {    const char* comma = strchr(names + 1, ',');    cout.write(names, comma - names) << " : " << arg1 << " | "; __f(comma + 1, args...);}
-vector<int> vec1,vec2,vec3,primes;
-int row,col;vector<vector<int>> vec;
-int n=0,m=0,k=0,n1=0,n2=0,n3=0,stt=0,ent=0,q=0,qry=0;
+#define For(i,l,r) for(int i=l;i<=r;i++)
+#define MAXN 101
+#define db(...) __f(#__VA_ARGS__, __VA_ARGS__)
 
-void Muneer(){
-    int ans=0,cnt=0,x=0,y=0,z=0,a=0,b=0,c=0;
-    string s;
-    int th,tm,h,m;
-
-
-
-
-    cin >> th >> tm >>s;
-
-    h=stoi(s.substr(0,2));
-    m=stoi(s.substr(3));
-
-    string hh="00";
-    string mm="00";
-
-    for(int i=m;i<tm;i++){
-        if(i==0||i==1||i==8||i==2||i==5){
-            mm="0"+to_string(i);
-            break;
-        }else if(i>=10 &&i/10==1||i/10==8||i/10==2||i/10==5){
-        if(i%10==0||i%10==1||i%10==8||i%10==2||i%10==5){
-            mm=to_string(i);
-            break;
-        }
-        }
-        
-    }
-
-    cout << hh+":"+mm<<endl;    
-    
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | "; __f(comma + 1, args...);
 }
-int32_t main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
-    int t=1;
-    cin >>t;    
-    while (t--)Muneer();
-    return 0;
+int T,N,a[MAXN],b[MAXN],c[MAXN];
+int main()
+{
+	cin>>T;
+	while(T--)
+	{
+		cin>>N;For(i,1,N)
+        cin>>a[i]>>b[i];
+        For(i,1,N)cin>>c[i];
+
+		int x=0;
+        For(i,1,N)
+        {
+            x+=a[i]-b[i-1]+c[i];
+            if(i<N)x=max(b[i],x+(b[i]-a[i]+1)/2);
+        }
+        cout<<x<<endl;
+	}
+	return 0;
 }
