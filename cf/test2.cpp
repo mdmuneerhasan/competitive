6eee3470f7c94b81bf700af898e82749e444cdd6
sharmin0312 <sharmin0312@gmail.com>
2021-03-15 18:22:41 +0530

    18:22-15-03-2021

--- cf/test2.cpp
+++ cf/test2.cpp
@@ -1,19 +1,34 @@
 #include<iostream>
 using namespace std;
+/* This code is submitted by Muneer Hasan
+* Computer-scince student ( @ Jamia Millia Islamia : New Delhi)
+* Email : md.muneerhasan@gmail.com
+* 21:04:04 Saturday 13-March:2021*/
+#include<bits/stdc++.h>
+using namespace std;
+#define int             long long int
+#define SF(v)           sort(v.begin(),v.end())
+#define UP(x)           for(int i=0;i<x.size();i++){if(x[i]<='z' && x[i]>='a')x[i]=char(x[i]-'a'+'A');}
+#define LO(x)           for(int i=0;i<x.size();i++){if(x[i]<='Z' && x[i]>='A')x[i]=char(x[i]-'A'+'a');}
+#define MAXHEAP         priority_queue<int>
+#define MINHEAP         priority_queue<int, vector<int>, greater<int> >
+#define SETBIT(x)       __builtin_popcountll(x)
+#define ZEROBIT(x)      __builtin_ctzll(x)
+#define PS(x,y)         fixed<<setprecision(y)<<x
+#define MOD             1000000007
+#define db(...)         __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }template <typename Arg1, typename... Args>void __f(const char* names, Arg1&& arg1, Args&&... args) {    const char* comma = strchr(names + 1, ',');    cout.write(names, comma - names) << " : " << arg1 << " | "; __f(comma + 1, args...);}
+vector<int> vec1,vec2,vec3,primes;
+int row,col;vector<vector<int>> vec;
+int n=0,m=0,k=0,n1=0,n2=0,n3=0,stt=0,ent=0,q=0,qry=0;
+string s, s1 ,s2;
 #define For(i,l,r) for(int i=l;i<=r;i++)
-#define MAXN 101
-#define db(...) __f(#__VA_ARGS__, __VA_ARGS__)
 
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args) {
-    const char* comma = strchr(names + 1, ',');
-    cout.write(names, comma - names) << " : " << arg1 << " | "; __f(comma + 1, args...);
-}
+#define MAXN 101
+
+
 int T,N,a[MAXN],b[MAXN],c[MAXN];
-int main()
-{
+int32_t main(){
 	cin>>T;
 	while(T--)
 	{
@@ -26,6 +41,7 @@ int main()
         {
             x+=a[i]-b[i-1]+c[i];
             if(i<N)x=max(b[i],x+(b[i]-a[i]+1)/2);
+            db(x);
         }
         cout<<x<<endl;
 	}
