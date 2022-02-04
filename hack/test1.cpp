cda3ff275336854d83116fa5e4bffb62c3299c6c
Muneer Hasan1 <muneer.hasan1@olaelectric.com>
2022-02-04 20:44:24 +0530

    20:44-04-02-2022

--- hack/test1.cpp
+++ hack/test1.cpp
@@ -1,68 +1,32 @@
-/* This code is submitted by Muneer Hasan
-* Computer-scince student ( @ Jamia Millia Islamia : New Delhi)
-* Email : md.muneerhasan@gmail.com
-* 22:48:31 Sunday 16-May:2021*/
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<iostream>
+#include<iostream>
+
 using namespace std;
-#define int             long long int
-#define SF(v)           sort(v.begin(),v.end())
-#define UP(x)           for(int i=0;i<x.size();i++){if(x[i]<='z' && x[i]>='a')x[i]=char(x[i]-'a'+'A');}
-#define LO(x)           for(int i=0;i<x.size();i++){if(x[i]<='Z' && x[i]>='A')x[i]=char(x[i]-'A'+'a');}
-#define MAXHEAP         priority_queue<int>
-#define MINHEAP         priority_queue<int, vector<int>, greater<int> >
-#define SETBIT(x)       __builtin_popcountll(x)
-#define ZEROBIT(x)      __builtin_ctzll(x)
-#define PS(x,y)         fixed<<setprecision(y)<<x
-#define MOD             1000000007
-#define N               100000
-#define db(...)         __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename Arg1>void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }template <typename Arg1, typename... Args>void __f(const char* names, Arg1&& arg1, Args&&... args) {    const char* comma = strchr(names + 1, ',');    cout.write(names, comma - names) << " : " << arg1 << " | "; __f(comma + 1, args...);}
-vector<int> vec1,vec2,vec3,primes;
-int row,col;vector<vector<int>> vec;
-int n=0,m=0,k=0,n1=0,target,n2=0,n3=0,stt=0,ent=0,q=0,qry=0;
-string s, s1 ,s2;
 
-
-void solve(int index,int sum,string ans){
-
-if(index==n1){
-
-    if(sum==target){
-
-        cout << ans<<endl;
-    }
-
-    return;
-}
-
-solve(index+1,sum+vec1[index],ans+" "+to_string(vec1[index]));
-
-solve(index+1,sum,ans);
-
-
-}
-void Muneer(){
-    int ans=0,cnt=0,x=0,y=0,a=0,b=0;
-    
-
-    cin >>n1;
-    vec1.clear();
-    vec1.resize(n1,0);
-    for(int q=0;q<n1;q++){
-      cin >> vec1[q];
-    }
-    sort(vec1.begin(),vec1.end());
-
-    cin >> target;
-
-     solve(0,0,"");
-    
-}
+int n,n1,n2,q=0,ans=0,cnt=0;
+vector<int> vec1;
 int32_t main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
-    int t=1;
-    // cin >>t;    
-    while (t--)Muneer();
+   int t;
+   //signature(true);
+    //cin >>t;    while (t--)
+    {   
+       
+       cin >>n1;
+       vec1.clear();
+       vec1.resize(n1,0);
+       for(int q=0;q<n1;q++){
+         cin >> vec1[q];
+       }
+       
+       for(int i=0;i<100;i++){
+           cout << i <<":"<<vec1[i] << ", ";
+       }
+       cout <<endl;
+       
+       
+       
+    }  
     return 0;
 }
