f780526531be04b4bfa0b36f3db7f8bc00e252b2
Muneer Hasan1 <muneer.hasan1@olaelectric.com>
2022-03-13 10:53:05 +0530

    10:53-13-03-2022

--- hack/test1.cpp
+++ hack/test1.cpp
@@ -1,32 +1,30 @@
+/* This code is submitted by Muneer Hasan
+* Computer-scince student ( @ Jamia Millia Islamia : New Delhi)
+* Email : md.muneerhasan@gmail.com
+* 14:05:13 Saturday 12-March:2022*/
 #include<iostream>
 #include<vector>
-#include<iostream>
-#include<iostream>
-
 using namespace std;
 
-int n,n1,n2,q=0,ans=0,cnt=0;
-vector<int> vec1;
-int32_t main(){
-   int t;
-   //signature(true);
-    //cin >>t;    while (t--)
-    {   
-       cin >>n1;
-       vec1.clear();
-       vec1.resize(n1,0);
-       for(int q=0;q<n1;q++){
-         cin >> vec1[q];
-       }
+int solve(string S){
+    
 
-       for(int i=0;i<100;i++){
-           cout << i <<":"<<vec1[i] << ", ";
-       }
-       cout <<endl;
-       
-       
-       
-       
-    }  
+}
+
+void Muneer(){
+    int ans=0,cnt=0,x=0,y=0,z=0,a=0,b=0,c=0;
+    
+    string s;
+    cin >> s;
+    cout << solve(s)<<endl;
+    
+    
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int t=1;
+    // cin >>t;    
+    while (t--)Muneer();
     return 0;
 }
